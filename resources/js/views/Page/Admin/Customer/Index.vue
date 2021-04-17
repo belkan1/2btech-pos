e0d66f857587bab1babd1e06d1e613b96ac91894
Hamza Sohail <mhamzasuhail@gmail.com>
2021-04-18 01:17:47 +0500

    changed customer

--- resources/js/views/Page/Admin/Customer/Index.vue
+++ resources/js/views/Page/Admin/Customer/Index.vue
@@ -94,7 +94,7 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Tambah Pengguna</h5>
+                        <h5 class="modal-title" id="exampleModalLabel">Add Customer</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Tutup">
                         <span aria-hidden="true">&times;</span>
                         </button>
